e1fdc3ebeddb9079bdb6374a4869aa387b902b90
João Lucas <jlgeno@varotti.org>
2021-11-21 18:27:37 +0100

    add linha e colunas

--- Codigo/Main.py
+++ Codigo/Main.py
@@ -1,8 +1,11 @@
-from pynput import keyboard
+from pynput import keyboard, mouse
 from pynput.keyboard import Key, Controller, Listener
 from machine import Pin
+import _thread
+from pynput.mouse import Button, Controller
 
-Teclado = Controller()
+Teclado = keyboard.Controller()
+Rato = mouse.Controller()
 
 #PinOuts
 Linha1 = Pin(0, Pin.IN)
@@ -24,6 +27,9 @@ b0 = Pin(12, Pin.OUT)
 b1 = Pin(13, Pin.OUT)
 b2 = Pin(14, Pin.OUT)
 b3 = Pin(15, Pin.OUT)
+
+#variaveis
+Camada = 0
 
 #detecta Locks
 num = 0
@@ -59,3 +65,122 @@ def Pressiona(key):
 
 with Teclado.Listener(on_press=Pressiona) as ouvidor:
     ouvidor.join()
+
+#Leitura das Linhas e Colunas
+if Linha1 == 1:
+    if Coluna1 == 1: Teclado.press(Key.num_lock) # toggle Num Lock
+#Troca de Camadas
+    elif Coluna2 == 1: Camada + 1
+    elif Coluna3 == 1: Camada - 1
+    elif Coluna4 == 1:
+        if num == 1: Teclado.press('/')
+        elif num == 0:
+            #if Camada == 0: Teclado.press(Key.media_play_pause)
+            #if Camada == 1: Teclado.press(Key.media_play_pause)
+            if Camada == 2:
+                Key.tap('/')
+                Key.tap('/')
+
+if Linha2 == 1:
+    if Coluna1 == 1:
+        if num == 1: Teclado.press(' 7')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.home)
+            elif Camada == 1: Teclado.press(Key.media_volume_mute)
+            elif Camada == 2: Teclado.press('(')
+    elif Coluna2 == 1:
+        if num == 1: Teclado.press('8')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.up)
+            elif Camada == 1: Teclado.press(Key.media_volume_up)
+            elif Camada == 2: Teclado.press(')')
+    elif Coluna3 == 1:
+        if num == 1: Teclado.press(' 9')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.page_up)
+            elif Camada == 1: Teclado.press(Key.media_volume_down)
+            elif Camada == 2: Teclado.press('<')
+    elif Coluna4 == 1:
+        if num == 1: Teclado.press('*')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.media_play_pause)
+            elif Camada == 1: Teclado.press(Key.media_play_pause)
+            elif Camada == 2: Teclado.press('>')
+
+if Linha3 == 1:
+    if Coluna1 == 1:
+        if num == 1: Teclado.press (' 4')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.left)
+            elif Camada == 1: Teclado.press(Key.f20)
+            elif Camada == 2: Teclado.press('{')
+    elif Coluna2 == 1:
+        if num == 1: Teclado.press('5')
+        elif num == 0:
+            if Camada == 0: Rato.click(Button.left, 1)
+            elif Camada == 1: Teclado.press(Key.media_previous)
+            elif Camada == 2: Teclado.press('}')
+    elif Coluna3 == 1:
+        if num == 1: Teclado.press('6')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.right)
+            elif Camada == 1: Teclado.press(Key.media_next)
+            elif Camada == 2: Teclado.press('"')
+    elif Coluna4 == 1:
+        if num == 1: Teclado.press('-')
+        elif num == 0:
+            #if Camada == 0: achar algo
+            #if Camada == 1: achar algo
+            if Camada == 2: Teclado.press("'")
+
+if Linha4 == 1:
+    if Coluna1 == 1:
+        if num == 1: Teclado.press('1')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.end)
+            elif Camada == 1: Teclado.press(Key.f17)
+            elif Camada == 2: Teclado.press('[')
+    if Coluna2 == 1:
+        if num == 1: Teclado.press('2')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.down)
+            elif Camada == 1: Teclado.press(Key.f16)
+            elif Camada == 2: Teclado.press(']')
+    if Coluna3 == 1:
+        if num == 1: Teclado.press('3')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.page_down)
+            elif Camada == 1: Teclado.press(Key.f19)
+            elif Camada == 2: Teclado.press('%')
+    if Coluna4 == 1:
+        if num == 1: Teclado.press('3')
+        elif num == 0:
+            #if Camada == 0: achar algo
+            if Camada == 1: Teclado.press(Key.f20)
+            elif Camada == 2: Teclado.press('#')
+
+if Linha5 == 1:
+    if Coluna1 == 1:
+        if num == 1: Teclado.press('0')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.insert)
+            elif Camada == 1: Teclado.press(Key.f13)
+            elif Camada == 2: Teclado.press(Key.tab)
+    if Coluna2 == 1:
+        if num == 1: Teclado.press(',')
+        elif num == 0:
+            #if Camada == 0: achar algo
+            if Camada == 1: Teclado.press(Key.f14)
+            elif Camada == 2: Teclado.press(',')
+    if Coluna3 == 1:
+        if num == 1: Teclado.press('.')
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.delete)
+            elif Camada == 1: Teclado.press(Key.f15)
+            elif Camada == 2: Teclado.press('.')
+    if Coluna4 == 1:
+        if num == 1: Teclado.press(Key.enter)
+        elif num == 0:
+            if Camada == 0: Teclado.press(Key.enter)
+            elif Camada == 1: Teclado.press(Key.f16)
+            elif Camada == 2: Teclado.press(Key.enter)
